b6357b79d3fd99607b23e990056380d993c332ed
Mehdi <mehdi@Sbns-MacBook-Pro.local>
2019-02-02 22:23:48 +0330

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>simplest manual react config</h2>
+        <p>let's go ahead!</p>
+      </div>
+    );
+  }
+}
